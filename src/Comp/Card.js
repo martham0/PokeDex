1d6ef3747e2222713e5f186cdba2fdd7e2fe83ff
martham0 <martha060997@gmail.com>
2023-06-14 11:26:07 -0700

    add images and fonts

--- src/Comp/Card.js
+++ src/Comp/Card.js
@@ -1,5 +1,6 @@
+import React from 'react';
 import './Card.css';
-import pok from './pok.png';
+import pok from './img/pok.png';
 import Tilt from 'react-tilt';
 
 function Card(props) {
@@ -8,9 +9,14 @@ function Card(props) {
       <div>
         <Tilt options={{ max: 38 }}>
           <div className="card">
-            <h2 className="poke">Name: {props.name}</h2>
-            <img className="photo" src={props.img} alt="" />
-            <h3> Type: {props.type}</h3>
+            <h2 className="poke"> {props.name}</h2>
+            <img width="200px" src={props.img} alt="" />
+            <h3>
+              {' '}
+              Type:
+              <br />
+              {props.type}
+            </h3>
           </div>
         </Tilt>
       </div>
@@ -19,8 +25,11 @@ function Card(props) {
     return (
       <Tilt className="Tilt" options={{ max: 38 }}>
         <div className="card">
-          <h2 className="poke">{props.name} could not be found</h2>
-          <img className="photo" src={pok} alt="" />
+          <h2 className="poke">
+            Sorry, we couldn't find {'\n'}
+            {props.name}. {'\n'}Try again.
+          </h2>
+          <img width="200px" src={pok} alt="" />
         </div>
       </Tilt>
     );
